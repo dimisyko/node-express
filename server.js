1cc4cb8b02668552354e3adefe462e6c076341df
dimisyko <dimitri.syko@hotmail.com>
2023-02-20 18:38:19 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const fs = require('fs'); 
+const data = JSON.parse(fs.readFileSync('./src/data/detail.json'))
+
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', 'src/pages')
+
+app
+  .use(express.static(__dirname + '/dist'))
+
+app.get('/', (req, res) => {
+    res.render('index', { data })
+  })
+
+app.get("/project/:id", (req, res) => {
+    const dataDetail = data.detailPage.find( data => data.page === req.params.id)
+    if (!dataDetail) res.status(404).render('404')
+    const projectIndex = data.detailPage.indexOf(dataDetail)
+    const next = data.detailPage[projectIndex + 1] ? data.detailPage[projectIndex + 1] : data.detailPage[0]
+    res.render('detail', { dataDetail, next })
+})
+app.use((req, res) => {
+    res.status(404).render('404')
+})
+
+app.listen(port)
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+entry: [
+  "./src/assets/js/script.js",
+  "./src/assets/style/style.css"
+],
+
+output: {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'js/script.js',
+},
+  module: {
+        rules: [
+          {
+            test: /\.pug$/,
+            use: [
+              {
+                loader: 'simple-pug-loader'
+              }
+            ]
+          },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              },
+          {
+            test: /\.(ttf|eot|otf|woff|woff2)$/,
+            type: 'asset/resource',
+            generator: {
+              filename: 'fonts/[name][ext]'
+            }
+          },
+          {
+            test: /\.(jpg|png|gif|webp|svg|ico|mp4)$/,
+            type: 'asset/resource',
+            generator: {
+              filename: 'img/[name][ext]'
+            }
+          },
+          {
+            test: /\.webmanifest$/i,
+            use: 'webpack-webmanifest-loader',
+            type: 'asset/resource',
+            generator: {
+                filename: './[contenthash:7][ext]'
+              }
+          },
+          {
+            test: /\.(sa|sc|c)ss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      'postcss-preset-env',
+                      'autoprefixer',
+                    ]
+                  ]
+                }
+              }
+            }, 'sass-loader']
+          }
+
+        ],
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: 'style/style.css'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {from: "src/assets/img", to: "img"}
+        ],
+      }),
+      ]
+};
