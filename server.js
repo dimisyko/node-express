6c37a605395aa4c4740baa200a46732fead615be
dimisyko <dimitri.syko@hotmail.com>
2023-02-26 23:35:17 +0100

    async functions

--- server.js
+++ server.js
@@ -1,29 +1,46 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
-const fs = require('fs'); 
-const data = JSON.parse(fs.readFileSync('./src/data/detail.json'))
-
+const logger = require('morgan')
+const { join } = require('path')
+const fs = require('fs').promises
 const port = process.env.PORT || 3000
+
+const urlFile = __dirname +'/src/data/'
+
+async function readJson(file){
+  try {
+    const data = await fs.readFile(join(urlFile, file), 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/pages')
 
 app
   .use(express.static(__dirname + '/dist'))
-
-app.get('/', (req, res) => {
+  .use(logger('dev'))
+  
+  app.get('/', async (req, res) => {
+    const data = await readJson("detail.json")
     res.render('index', { data })
   })
-app.get("/project/:id", (req, res) => {
-    const dataDetail = data.detailPage.find( data => data.page === req.params.id)
-    if (!dataDetail) res.status(404).render('404')
-    const projectIndex = data.detailPage.indexOf(dataDetail)
-    const next = data.detailPage[projectIndex + 1] ? data.detailPage[projectIndex + 1] : data.detailPage[0]
-    res.render('detail', { dataDetail, next })
+
+app.get("/project/:id", async (req, res) => {
+  const data = await readJson("detail.json")
+    const detail = data.detailPage.find( data => data.page === req.params.id)
+    if (!detail) res.status(404).render('404')
+    const projectIndex = data.detailPage.indexOf(detail)
+    const nextProject = data.detailPage[projectIndex + 1] ? data.detailPage[projectIndex + 1] : data.detailPage[0]
+    const prevProject = data.detailPage[projectIndex - 1] ? data.detailPage[projectIndex - 1] : data.detailPage[data.detailPage.length - 1]
+    res.render('detail', { detail, nextProject, prevProject })
 })
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render("404")
 })
 
 app.listen(port)
