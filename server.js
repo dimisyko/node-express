a6b861ef22e3d4fe69bb69586305e0b95b23d89a
dimisyko <91122888+dimisyko@users.noreply.github.com>
2023-02-28 11:29:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const urlFile = __dirname +'/src/data/'
 
 async function readJson(file){
   try {
-    const data = await fs.readFile(join(urlFile, file), 'utf8');
-    return JSON.parse(data);
+    const dataJson = await fs.readFile(join(urlFile, file), 'utf8');
+    return JSON.parse(dataJson);
   } catch (err) {
-    console.log(err);
+      res.status(500).send({ message : err.message || "Erreur"})
   }
 }
 
@@ -26,18 +26,25 @@ app
   .use(logger('dev'))
   
   app.get('/', async (req, res) => {
-    const data = await readJson("detail.json")
-    res.render('index', { data })
+    try{
+       const data = await readJson("detail.json")
+       res.render('index', { data })
+    } catch (err) {
+        res.status(500).send({ message : err.message || "Erreur"})
+  }
   })
 
 app.get("/project/:id", async (req, res) => {
-  const data = await readJson("detail.json")
+  try{
+    const data = await readJson("detail.json")
     const detail = data.detailPage.find( data => data.page === req.params.id)
-    if (!detail) res.status(404).render('404')
     const projectIndex = data.detailPage.indexOf(detail)
     const nextProject = data.detailPage[projectIndex + 1] ? data.detailPage[projectIndex + 1] : data.detailPage[0]
     const prevProject = data.detailPage[projectIndex - 1] ? data.detailPage[projectIndex - 1] : data.detailPage[data.detailPage.length - 1]
     res.render('detail', { detail, nextProject, prevProject })
+  }catch {
+    res.status(404).render("404")
+  }
 })
 app.use((req, res) => {
     res.status(404).render("404")
